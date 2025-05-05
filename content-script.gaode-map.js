9800f2a7e0c09612cda4951b0df7d7d3b4943543
novlan1 <1576271227@qq.com>
2025-05-06 01:30:45 +0800

    feat: add gaode map

---
+++ content-script.gaode-map.js
@@ -0,0 +1,142 @@
+// 注意，必须设置了run_at=document_start 此段代码才会生效
+document.addEventListener('DOMContentLoaded', () => {
+  main();
+});
+const justHold = time => new Promise(resolve => setTimeout(resolve, time));
+
+
+function getRandomNumber(min, max) {
+  const result = Math.random() * (max - min) + min;
+  return result;
+}
+
+
+async function saveData(data, fileName) {
+  const blob = new Blob([data], { type: 'text/plain' });
+  const url = URL.createObjectURL(blob);
+
+  // 创建下载链接并触发点击
+  const a = document.createElement('a');
+  a.href = url;
+  a.download = fileName;
+  a.click();
+}
+
+async function getList() {
+  const list = [...document.querySelectorAll('.poilist li')];
+  console.log('list', list);
+
+  const data = list.map((item) => {
+    // console.log('item', item)
+    const name = item.querySelector('.ml_30 .row span a')?.innerText.trim();
+    const address = item.querySelector('.ml_30 .row.addr span')?.innerText.trim();
+    const phone = item.querySelector('.ml_30 .row.tel')?.innerText?.trim() || '';
+    const info = {
+      name,
+      address,
+      phone,
+    };
+    // console.log('info', info)
+    return info;
+  });
+  return data;
+}
+
+async function goNext() {
+  const nextButton = document.querySelector('a[tid=toNextPage]');
+  console.log('nextButton', nextButton);
+  if (nextButton && !nextButton.className.includes('next-none')) {
+    nextButton.click();
+    return true;
+  }
+  return false;
+}
+
+async function main() {
+  await justHold(getRandomNumber(2000, 4000));
+  const TOTAL = 369;
+  const START_PAGE = 0;
+  for (let i = START_PAGE;i < TOTAL;i++) {
+    const cityName = await switchCity(i);
+    return;
+    const allList = [];
+    await loopFetch(allList, cityName || `city-${i}`);
+  }
+}
+
+
+async function switchCity(cityIndex = 0) {
+  console.log('try switch city start...');
+  const btn = document.querySelector('.city-title.usel');
+  console.log('city title btn', btn);
+  if (btn) {
+    btn.click();
+  }
+
+  await justHold(getRandomNumber(1000, 2000));
+  const cityButton = document.querySelector('.city-city-title');
+  console.log('[cityButton]', cityButton);
+  if (cityButton) {
+    cityButton.click();
+  }
+
+  await justHold(getRandomNumber(1000, 2000));
+  const allCities = [...document.querySelectorAll('li[adcode]')];
+  console.log('[allCities]', allCities);
+
+  const targetCity = allCities?.[cityIndex];
+  console.log('[targetCity]', targetCity);
+  if (targetCity) {
+    targetCity.click();
+  }
+
+  await justHold(getRandomNumber(2000, 5000));
+  const searchInput = document.querySelector('.iptbox input');
+  console.log('[searchInput]', searchInput);
+  if (searchInput) {
+    searchInput.click();
+  }
+  await justHold(getRandomNumber(2000, 5000));
+
+  searchInput.value = '网吧';
+
+  await justHold(getRandomNumber(1000, 2000));
+  const searchButton = document.querySelector('.iptbox input');
+  console.log('[searchButton]', searchButton);
+  searchButton?.click();
+  await justHold(getRandomNumber(2000, 4000));
+
+  // const firstSuggestItem = document.querySelector('.ui3-suggest-item')
+  // console.log('[firstSuggestItem]', firstSuggestItem)
+  // firstSuggestItem?.click();
+
+  return targetCity?.innerText?.trim();
+}
+
+
+async function loopFetch(allList, cityName) {
+  await justHold(getRandomNumber(2000, 4000));
+  const list = await getList();
+  allList.push(...list);
+
+  const hasNext = await goNext();
+  console.log('hasNext', hasNext);
+  console.log('allList.length', allList.length);
+  if (!hasNext) {
+    if (allList.length) {
+      saveData(JSON.stringify(allList), `baidu-map-${cityName}.json`);
+    } else {
+      console.log('数据为空，无需下载');
+    }
+  } else {
+    await loopFetch(allList, cityName);
+  }
+}
+
+
+// function getGetCityList() {
+//   const allCities = [...document.querySelectorAll('tbody tr a')];
+//   console.log('[allCities]', allCities);
+//   const cityNames = allCities.map(item => item.innerText?.trim());
+//   console.log('cityNames', JSON.stringify(cityNames));
+// }
